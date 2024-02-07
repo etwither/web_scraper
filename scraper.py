f6d195a1f6917e5dd0daf30696681d59ebf36001
etwither <etwitherspoon76@gmail.com>
2024-02-07 10:58:28 -0600

    HTML parser added

--- scraper.py
+++ scraper.py
@@ -3,5 +3,11 @@ from bs4 import BeautifulSoup
 
 #getting the URL
 url = input("Enter URL: ")
+
+#get the webpage
 response = requests.get(url)
 html_content = response.content
+
+#parse the response
+temp = BeautifulSoup(html_content, 'html.parser')
+
